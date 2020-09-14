0fe5c69b74462ec7222f704f78a59999729e3fc3
Akshath <akshathmahajan13@gmail.com>
2020-09-14 18:51:10 +0530

    Minor bug fix

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 
 export class Sort extends Component {
-    state = {ascending: this.props.ascending}
     render() {
         return (
             <div className="col-12">
                 <button type="button" className="btn btn-block btn-outline-success sort-btn" onClick={this.props.sortData}>
-                    Change order to - {this.state.ascending?'Descending':'Ascending'}
+                    Change order to - {this.props.ascending?'Descending':'Ascending'}
                 </button>
             </div>
         )
